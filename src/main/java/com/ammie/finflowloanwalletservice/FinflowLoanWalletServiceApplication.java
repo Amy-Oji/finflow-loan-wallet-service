12dcd5d4ab6677864a50abca17f39883763b97ba
amy-oji <amyojiakor@gmail.com>
2025-12-09 12:10:58 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/ammie/finflowloanwalletservice/FinflowLoanWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ammie.finflowloanwalletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinflowLoanWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinflowLoanWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ammie/finflowloanwalletservice/FinflowLoanWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ammie.finflowloanwalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinflowLoanWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
